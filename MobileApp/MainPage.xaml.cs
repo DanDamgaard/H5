12dd8ec326d7c9c28456aa53653ae8fcf14bc326
DanDamgaard <65528625+DanDamgaard@users.noreply.github.com>
2023-11-16 13:41:41 +0100

    first push
    push api + maui projects to repository

---
+++ MobileApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using static System.Net.WebRequestMethods;
+
+namespace MobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+    
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCallApiBtnClicked(object sender, EventArgs e)
+        {
+            var httpClient = new HttpClient();
+            var baseUrl = DeviceInfo.Platform == DevicePlatform.Android
+                 ? "http://10.0.2.2:5131"
+                 : "http://localhost:5131";
+
+           var response = await httpClient.GetAsync($"{baseUrl}/WeatherForecast");
+
+            var data = await response.Content.ReadAsStringAsync();
+        }
+  
+    }
+
+}
