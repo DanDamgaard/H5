9ad3f7d5616feede7818a8df45d50a5b179f5c55
dalya <dalyalhamami@gmail.com>
2023-11-29 15:08:41 +0100

    Adding UserBook

---
+++ API/Controllers/UserBookController.cs
@@ -0,0 +1,54 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserBookController : ControllerBase
+    {
+        private readonly AppDbContext appDbContext;
+
+        public UserBookController(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        // Rent a book
+        [HttpPost("RentBook")]
+        public async Task<ActionResult<UserBook>> RentBook(UserBook userBook)
+        {
+            if (userBook != null)
+            {
+                // Ensure that the book and user exist
+                var bookExists = await appDbContext.Book.AnyAsync(b => b.Id == userBook.BookId);
+                var userExists = await appDbContext.User.AnyAsync(u => u.Id == userBook.UserId);
+
+                if (bookExists && userExists)
+                {
+                    // Check if the user has already rented the same book
+                    var existingRental = await appDbContext.UserBook
+                        .FirstOrDefaultAsync(ub => ub.BookId == userBook.BookId && ub.UserId == userBook.UserId);
+
+                    if (existingRental != null)
+                    {
+                        return BadRequest("User has already rented this book");
+                    }
+
+                    // Set the EndDate to StartDate + 14 days
+                    userBook.EndDate = userBook.StartDate.AddDays(14);
+
+                    var result = appDbContext.UserBook.Add(userBook).Entity;
+                    await appDbContext.SaveChangesAsync();
+                    return Ok(result);
+                }
+                return BadRequest("Invalid Book or User");
+            }
+            return BadRequest("Invalid Request");
+        }
+
+    }
+}
---
+++ API/Models/UserBook.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class UserBook
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, ForeignKey("User")]
+        public int UserId { get; set; }
+
+        [Required, ForeignKey("Book")]
+        public int BookId { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime EndDate { get; set; }
+    }
+}
