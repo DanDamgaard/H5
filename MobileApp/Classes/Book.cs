3418ba7716a8f1e5cb9607efb6d74bba13c3baf9
DanDamgaard <65528625+DanDamgaard@users.noreply.github.com>
2023-12-04 12:27:29 +0100

    added auther to filter & added userRentHistoryPage

--- MobileApp/Classes/Book.cs
+++ MobileApp/Classes/Book.cs
@@ -13,22 +13,20 @@ namespace MobileApp.Classes
         private string _category;
         private string _description;
         private string _image;
-        private int _authorId;
+        private string _authorName;
         private string _publisher;
         private int _status;
-        private Author _author;
 
-        public Book(int id, string title, string category, string description, string image, int authorId, string publisher, int status, Author author)
+        public Book(int id, string title, string category, string description, string image, string authorName, string publisher, int status)
         {
             _id = id;
             _title = title;
             _category = category;
             _description = description;
             _image = image;
-            _authorId = authorId;
             _publisher = publisher;
             _status = status;
-            _author = author;
+            _authorName = authorName;
         }
 
         public int Id { get { return _id; } set { _id = value; } }
@@ -36,10 +34,10 @@ namespace MobileApp.Classes
         public string Category { get { return _category; } set { _category = value; } }
         public string Description { get { return _description; } set { _description = value; } }
         public string Image { get { return _image; } set { _image = value; } }
-        public int AuthorId { get { return _authorId; } set { _authorId = value; } }
+        public string AuthorName { get { return _authorName; } set { _authorName = value; } }
         public string Publisher { get { return _publisher; } set { _publisher = value; } }
         public int Status { get { return _status; } set { _status = value; } }
-        public Author Author { get { return _author; } set { _author = value; } }
+
 
     }
 }
